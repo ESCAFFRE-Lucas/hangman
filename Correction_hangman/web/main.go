ea10dfab6b5c92db8a4dcff79b191183f526205d
LESCAFFRE <lucas.escaffre@ynov.com>
2022-01-05 17:00:54 +0100

    change the url from .../gothml to .../hangman, change the submit button and print a word with full dash

--- Correction_hangman/web/main.go
+++ Correction_hangman/web/main.go
@@ -16,19 +16,16 @@ func Hangman(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("index.gohtml"))
 	_ = tmpl.Execute(w, struct {
 		Title string
-		Data  []string
-	}{Title: "ok", Data: []string{classic.GetRandomWord(), "yo", " ", "p", "d"}},
+		Data  string
+	}{Title: "ok", Data: classic.HideLetters(classic.GetRandomWord())},
 	)
 
-	switch r.Method {
-	case "POST":
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprintf(w, "ParseForm() err : %v", err)
-			return
-		}
-		letter := r.FormValue("letter")
-		fmt.Fprintf(w, "Letter = %s\n", letter)
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err : %v", err)
+		return
 	}
+	letter := r.FormValue("letter")
+	fmt.Fprintf(w, "Letter = %s\n", letter)
 }
 
 func main() {
@@ -36,10 +33,10 @@ func main() {
 	server := http.NewServeMux()
 	// url http://localhost:8000/
 	server.HandleFunc("/", Home)
-	server.HandleFunc("/gohtml", Hangman)
+	server.HandleFunc("/hangman", Hangman)
 
 	server.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
 	// listen to the port 8000
-	fmt.Println("server listening on http://localhost:8000/gohtml")
+	fmt.Println("server listening on http://localhost:8000/hangman")
 	http.ListenAndServe(":8000", server)
 }
