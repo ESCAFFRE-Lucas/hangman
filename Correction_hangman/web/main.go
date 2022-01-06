4dcf1e3c5a1a893d3fa66d0c6e2b8337e913ebc8
LESCAFFRE <lucas.escaffre@ynov.com>
2022-01-06 15:46:39 +0100

    Prevent the word from changing when entering a letter with the data of the server

--- Correction_hangman/web/main.go
+++ Correction_hangman/web/main.go
@@ -15,20 +15,11 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func Hangman(w http.ResponseWriter, r *http.Request) {
-	target := classic.GetRandomWord()
-	tmpl := template.Must(template.ParseFiles("index.gohtml"))
-	err := tmpl.Execute(w, structure.Stock{
-		Title: "Hangman", Right: []string{}, Wrong: []string{}, Attempts: 10, TargetWord: target, CurrentWord: classic.InitWord(target)},
-	)
-	if err != nil {
-		fmt.Println(err)
-	}
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err : %v", err)
 		return
 	}
-	letter := r.FormValue("letter")
-	fmt.Fprintf(w, "letter :%s\n", letter)
+	http.Redirect(w, r, "../", http.StatusSeeOther)
 }
 
 func manager() structure.Stock {
@@ -58,5 +49,6 @@ func main() {
 	server.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
 	// listen to the port 8000
 	fmt.Println("server listening on http://localhost:8000/")
+
 	http.ListenAndServe(":8000", server)
 }
